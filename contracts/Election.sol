dee7714086c5b1764b889434b5f805a88200176b
nonce <noncedeleon@protonmail.com>
2018-10-20 13:54:15 +0200

    2. List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,49 @@ pragma solidity ^0.4.2;
 
 contract Election {
 
-	string public candidate;
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
-	function Election () public {
+	// mapping(key => value)
+		// key CAN'T be:
+			// mapping
+			// dynamically sized array
+			// contract
+			// enum
+			// struct
+	
+	// Store & fetch candidates
+	mapping(uint => Candidate) public candidates;
 
-		candidate = "Candidate 1";
+	uint public candidatesCount;
 
+	// Store accounts that have voted
+	mapping(address => bool) public voters;
+
+	function Election() public{
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote (uint _candidateID) public {
+		// require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateID > 0 && _candidateID <= candidatesCount);
+
+		// record that voter has voted
+		voters[msg.sender] = true;
+
+		// update candidate vote count
+		candidates[_candidateID].voteCount ++;
 	}
 }
